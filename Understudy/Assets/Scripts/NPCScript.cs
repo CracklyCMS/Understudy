06ce68a515257582d6084dcc844a7131bd2fd202
Cass <cracklycms@gmail.com>
2024-07-13 11:30:37 -0500

    Custom NPC icons.

--- Understudy/Assets/Scripts/NPCScript.cs
+++ Understudy/Assets/Scripts/NPCScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.UI;
 
 public class NPCScript : MonoBehaviour
 {
@@ -10,11 +11,14 @@ public class NPCScript : MonoBehaviour
     public string dialogue;
     public TextMeshProUGUI text;
     public Canvas textBox;
+    public Image iconComponent;
+    public Sprite icon;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         textBox.gameObject.SetActive(true);
         text.text = dialogue;
+        iconComponent.sprite = icon;
         print("DETECTED");
     }
 
