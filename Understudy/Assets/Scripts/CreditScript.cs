6a57418eb3e4e3d75066578e8141c8e58ae69b14
May8Craw <166954828+May8Craw@users.noreply.github.com>
2024-08-21 00:01:07 -0400

    added credits (not in the game yet!)
    added credits scene and credits buttons to and from credits scene

---
+++ Understudy/Assets/Scripts/CreditScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
